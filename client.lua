21de4f61b45b5e3de96fc3abab0c6cb8d58070fb
fxDopa <131190297+rdopaa@users.noreply.github.com>
2023-08-07 23:38:52 -0300

    Update client.lua
    Minor Fixes

--- client.lua
+++ client.lua
@@ -1,13 +1,11 @@
 ESX = nil
-local PlayerData = {}
+local PlayingAnim = false
 
 Citizen.CreateThread(function()
     while ESX == nil do
         ESX = exports['es_extended']:getSharedObject()
         Citizen.Wait(0)
     end
-
-    Citizen.Wait(0)
 
     ESX.PlayerData = ESX.GetPlayerData()
 end)
@@ -22,39 +20,44 @@ function LoadAnimDict(dict)
     end
 end
 
-RegisterCommand('robar', function() -- You can change the name of the command here.
+RegisterCommand(Config.Command, function()
     local player = PlayerPedId()
     if IsPedArmed(player, 4) then
-        local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
-        if closestPlayer ~= -1 and closestDistance <= 2.0 then
+        local closestDistance = ESX.Game.GetClosestPlayer()
+        if closestDistance <= 1.5 then
+            local closestPlayer = ESX.Game.GetClosestPlayer()
             local closestPlayerPed = GetPlayerPed(closestPlayer)
             local closestPlayerHasHandsUp = IsEntityPlayingAnim(closestPlayerPed, "random@mugging3", "handsup_standing_base", 3)
             LoadAnimDict("mini@repair")
             if closestPlayerHasHandsUp or IsPlayerDead(closestPlayer) then
-                openNearbyInventory(closestPlayer)
                 ExecuteCommand('me lo cachea')
                 TaskPlayAnim(PlayerPedId(), "mini@repair", "fixing_a_ped", 1.0, -1.0, -1, 49, 0, false, false, false)
+                PlayingAnim = true
+                openNearbyInventory(closestPlayer)
                 Citizen.Wait(3000)
 
                 ClearPedTasks(player)
                 ClearPedSecondaryTask(player)
             else
-                ESX.ShowNotification('El jugador no tiene las manos levantadas')
+                PlayingAnim = false
+                ESX.ShowNotification('El jugador no tiene las manos levantadas') -- CAN CHANGE
             end
         else
-            ESX.ShowNotification('No hay jugadores cercanos para robar')
+            PlayingAnim = false
+            ESX.ShowNotification('No hay jugadores cercanos para robar') -- CAN CHANGE
         end
     else
-        ESX.ShowNotification('Debes tener un arma de fuego para robar a otra persona')
+        PlayingAnim = false
+        ESX.ShowNotification('Debes tener un arma de fuego para robar a otra persona') -- CAN CHANGE
     end
 end)
 
 function openNearbyInventory(closestPlayer)
-    local ox_inventory = ESX.GetConfig().OxInventory
     local closestPlayer = ESX.Game.GetClosestPlayer()
-    if closestPlayer ~= -1 then
+    if (PlayingAnim == true) then
         exports.ox_inventory:openInventory('player', GetPlayerServerId(closestPlayer))
     end
 end
 
-RegisterKeyMapping('robar', 'Robar a un jugador cercano', 'keyboard', 'X') -- You can Change the Key Binding Here.
+RegisterKeyMapping('robar', 'Robar a un jugador cercano', 'keyboard', 'X')
+
