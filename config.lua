942f32125085838d49d286b0ea12ff56a587913e
fxDopa <131190297+rdopaa@users.noreply.github.com>
2024-05-25 05:09:01 -0300

    Added more config.

--- config.lua
+++ config.lua
@@ -3,3 +3,5 @@ Config = {} or Config
 Config.Command = 'robar'
 Config.CommandChat = true
 Config.CommandText = 'me lo cachea'
+
+Config.StealMode = "HandsUp" -- Steal Mode: "HandsUp" (Only Handsup), "Dead" (Only Dead), "Both" (Both options Dead and Handsup)
